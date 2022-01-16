63b3050e8cdb9b38392b765c939bac21dd32d79a
Hiury Bressanelle <contatobressanelli@gmail.com>
2022-01-16 19:11:14 -0300

    feat: add microFE

---
+++ apps/mfe1/src/app/remote-entry/entry.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { RemoteEntryComponent } from './entry.component';
+
+@NgModule({
+  declarations: [RemoteEntryComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: RemoteEntryComponent,
+      },
+    ]),
+  ],
+  providers: [],
+})
+export class RemoteEntryModule {}
